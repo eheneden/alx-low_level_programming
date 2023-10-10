09fbb19fcf861bf2d0b7f6f07991b9e0c70da430
eheneden <peter.osaigbovo@gmail.com>
2023-10-10 01:13:12 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistdi0.h>
+/**
+ * main - Entry
+ * Return: (1) sucess
+ */
+int main (void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
