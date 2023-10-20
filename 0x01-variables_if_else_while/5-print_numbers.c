d3befe268c29084b8012669c2661489f5909dbfc
eheneden <peter.osaigbovo@gmail.com>
2023-10-20 04:54:40 -0700

    numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single digit numbers of base
+ *
+ * Return: Always (0) success
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
+
