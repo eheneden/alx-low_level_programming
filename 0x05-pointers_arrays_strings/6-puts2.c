4f35e8106793d8cf3492315d3647770736204faa
eheneden <peter.osaigbovo@gmail.com>
2024-07-28 10:48:55 -0700

    one character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,56 +1,24 @@
 #include "main.h"
 
 /**
-
  * puts2 - function should print only one character out of two
-
  * starting with the first one
-
- * @str: input
-
- * Return: print
-
+ * @str: input string
+ * Return: void
  */
-
 void puts2(char *str)
-
 {
+	int i = 0;
 
-        int longi = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
 
-        int t = 0;
-
-        char *y = str;
-
-        int o;
-
-
-        while (*y != '\0')
-
-        {
-
-                y++;
-
-                longi++;
-
-        }
-
-        t = longi - 1;
-
-        for (o = 0 ; o <= t ; o++)
-
-        {
-
-                if (o % 2 == 0)
-
-        {
-
-                _putchar(str[o]);
-
-        }
-
-        }
-
-        _putchar('\n');
-
+	_putchar('\n');
 }
+
