5ecea0cd90ad6e5fd80924d414eb750787709999
eheneden <peter.osaigbovo@gmail.com>
2024-08-09 08:55:39 -0700

    updates

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverse array
+ * @a: array of integers
+ * @n: number of elements in array
+ * Return: void
+ */
+
+void reverse_array(int *a, int n);
+
+{
+
+	int half = n / 2;
+	int left, right, i;
+
+	n -= 1;
+
+	for (i = 0; i < half; i++)
+	{
+		left = a[n - i];
+		right = a[i];
+		a[i] = left;
+		a[n - i] = right;
+	}
+
+}
+
+
+
